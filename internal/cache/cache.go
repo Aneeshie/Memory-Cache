7273084eaf7ef4dec7afe4b3f5611720fa2280ab
Aneeshie <aneeshdas556@gmail.com>
2026-09-05 18:07:12 +0530

    feat: implement the put method for cache

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -30,3 +30,24 @@ func (c *Cache) Get(key string) (*CachedEntry, error) {
 
 	return hit, nil
 }
+
+func (c *Cache) Put(key string, data []byte) {
+	//construct the heap node first.
+	Node := &HeapNode{
+		DataID:   key,
+		LastUsed: time.Now(),
+	}
+
+	//after this the node has the HeapIndex filled in now...
+	c.heap.Insert(Node)
+
+	//now the entry
+	entry := &CachedEntry{
+		Data:     data,
+		LastUsed: Node.LastUsed,
+		Node:     Node,
+	}
+
+	//push it into the map now
+	c.cacheMap[key] = entry
+}
